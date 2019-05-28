86c0999986b6ee100f6d8290f3aca9be9c969eda
HuyTran1996 <huytran@monsterpixel.us>
2019-05-28 22:24:09 +0700

    filter UI

--- src/page/main/component/header/header.js
+++ src/page/main/component/header/header.js
@@ -1,16 +1,72 @@
 import React ,{ Component } from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Modal } from 'react-bootstrap';
 import '../header/header.css';
 import { Link } from 'react-router-dom';
 import { localStorageUserKey } from './../../../../share/constant';
+import axios from 'axios';
+import { enviroment } from '../../../../core/enviroment';
 class Header extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleClose = this.handleClose.bind(this);
+        this.getDataCountry();
+    }
     data = JSON.parse(localStorage.getItem(localStorageUserKey));
     signout() {
         localStorage.clear();
     }
 
+    state = {
+        isShowModal: false,
+        country:[],
+        city:[]
+    }
+
+    handleClose() {
+        this.setState({ isShowModal: false });
+    }
+
+    handleShow() {
+        this.setState({
+            isShowModal: true
+        })
+    }
+
+    getDataCountry() {
+        const token = JSON.parse(localStorage.getItem('userStorage')).token;
+        axios.get(enviroment + 'places/' , {headers : {
+            Authorization: 'Bearer ' + token
+        }})
+        .then((data) =>{
+            this.setState({
+                country: data.data.data
+            })
+        })
+        .catch((error) =>{
+            this.props.onGetCity([]);
+        })
+    }
+
     render() {
+        let cityDOM = this.state.city.length > 0 ? 
+        (
+            <Form.Group controlId="location">
+                <Form.Control
+                    as="select">
+                    {this.state.location.map((element , index) =>{
+                        return (
+                            <option
+                            key={index}
+                            value={element.name}
+                            >{element.name}</option>
+                        )
+                    })}
+                </Form.Control>
+            </Form.Group>
+        ) : (
+            <div></div>
+        )
         return (
             <div className="header-container">
                 <Form.Group controlId="finding-bar" className="header-input">
@@ -19,6 +75,7 @@ class Header extends Component {
                         placeholder="type your friend or location you want to know" 
                         onChange={(event) => this.handleInputChange(event , 'username')}/>
                 </Form.Group>
+                
                 <div className="header-btn">
                     <div className="header-btn-item">
                         <Link
@@ -34,7 +91,7 @@ class Header extends Component {
                         }}>Home</Link>
                     </div>
                     <div className="header-btn-item">Setting</div>
-                    <div className="header-btn-item">Filter</div>
+                    <div className="header-btn-item" onClick={()=> this.handleShow()}>Filter</div>
                     <div className="header-btn-item">
                         <Link
                         onClick={this.signout.bind(this)} 
@@ -44,6 +101,30 @@ class Header extends Component {
                         Sign out
                         </Link>
                     </div>
+                </div>
+
+                <div>
+                    <Modal show={this.state.isShowModal} onHide={this.handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Filter</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form.Group controlId="city">
+                                <Form.Control
+                                    as="select">
+                                    {this.state.country.map((element , index) =>{
+                                    return (
+                                        <option
+                                        key={index}
+                                        value={element.name}
+                                        >{element.name}</option>
+                                    )
+                                    })}
+                                </Form.Control>
+                                {cityDOM}
+                            </Form.Group>
+                        </Modal.Body>
+                    </Modal>
                 </div>
             </div>
         )
